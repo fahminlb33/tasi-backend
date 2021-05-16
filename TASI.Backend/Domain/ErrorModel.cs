d9a8d6fd62773405ec7764adf72d963fd1602e2a
fahminlb33 <fahminlb33@gmail.com>
2021-05-16 20:17:44 +0700

    Abstract out error and response model

---
+++ TASI.Backend/Domain/ErrorModel.cs
@@ -0,0 +1,6 @@
+﻿using TASI.Backend.Infrastructure.Resources;
+
+namespace TASI.Backend.Domain
+{
+    public record ErrorModel (string Message, ErrorCodes AppCode, object Data = null);
+}
---
+++ TASI.Backend/Infrastructure/Resources/ErrorMessages.cs
@@ -0,0 +1,24 @@
+﻿using TASI.Backend.Domain;
+
+namespace TASI.Backend.Infrastructure.Resources
+{
+    public enum ErrorCodes
+    {
+        Unauthorized,
+        Forbidden,
+        ModelValidation,
+        UnhandledException,
+        NameIdentifierIsEmpty
+    }
+
+    public static class ErrorMessages
+    {
+        public const string Unauthorized = "You are unauthorized to access this resource";
+        public const string Forbidden = "You don't have sufficent permission to access this resource";
+        public const string ModelValidation = "Request body/params/query contains invalid value";
+        public const string UnhandledException = "An error has occurred when executing you request";
+        public const string NameIdentifierNull = "Unable to get name identifier claim from Authorization header";
+
+        public static ErrorModel InternalExceptionModel = new(UnhandledException, ErrorCodes.ModelValidation);
+    }
+}
