dc43da6453f97043e471968e25e1f6bb6e5c542c
fahminlb33 <fahminlb33@gmail.com>
2021-05-22 10:53:55 +0700

    Bugfix division by zero

--- TASI.Backend/Domain/Pagination.cs
+++ TASI.Backend/Domain/Pagination.cs
@@ -5,7 +5,7 @@ namespace TASI.Backend.Domain
 {
     public record Pagination<T>(int Page, int Size, int TotalRecords, IList<T> Data)
     {
-        public int TotalPages => (int) Math.Ceiling(TotalRecords / (double) Size);
+        public int TotalPages => Size == 0 ? 0 : (int) Math.Ceiling(TotalRecords / (double) Size);
 
         public bool HasPrevious => Page > 1;
         public bool HasNext => Page < TotalPages - 1;
