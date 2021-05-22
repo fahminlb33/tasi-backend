48559f8163e10c988523f74ed534b0f376214bd5
fahminlb33 <fahminlb33@gmail.com>
2021-05-22 08:50:13 +0700

    Add supplier get all route

---
+++ TASI.Backend/Controllers/SuppliersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TASI.Backend.Domain.Users.Handlers;
+using TASI.Backend.Infrastructure.Resources;
+
+namespace TASI.Backend.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SuppliersController : ControllerBase
+    {
+        private readonly ILogger<SuppliersController> _logger;
+        private readonly IMediator _mediator;
+
+        public SuppliersController(IMediator mediator, ILogger<SuppliersController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSuppliers([FromQuery] GetUsersCommand model)
+        {
+            try
+            {
+                return await _mediator.Send(model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error in {0}", HttpContext.Request.Path);
+                return StatusCode((int)HttpStatusCode.InternalServerError, ErrorMessages.InternalExceptionModel);
+            }
+        }
+    }
+}
---
+++ TASI.Backend/Domain/Suppliers/Handlers/GetSuppliersCommand.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TASI.Backend.Domain.Suppliers.Entities;
+using TASI.Backend.Infrastructure.Database;
+
+namespace TASI.Backend.Domain.Suppliers.Handlers
+{
+    public class GetSuppliersCommand : IRequest<IActionResult>
+    {
+        public int Page { get; set; }
+        public int Limit { get; set; } = 10;
+        public string? Query { get; set; }
+    }
+
+    public class GetSuppliersCommandHandler : IRequestHandler<GetSuppliersCommand, IActionResult>
+    {
+        private readonly ILogger<GetSuppliersCommandHandler> _logger;
+        private readonly TasiContext _context;
+
+        public GetSuppliersCommandHandler(ILogger<GetSuppliersCommandHandler> logger, TasiContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Handle(GetSuppliersCommand request, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("Finding suppliers with query {0}", request.Query);
+
+            var skipCount = request.Page * request.Limit;
+            var query = _context.Suppliers.AsQueryable();
+            if (request.Query != null)
+            {
+                query = query.Where(x => EF.Functions.Like(x.Name, $"%{request.Query}%"))
+                    .Where(x => EF.Functions.Like(x.Address, $"%{request.Query}%"));
+            }
+
+            var result = await query.Skip(skipCount).Take(request.Limit).ToListAsync(cancellationToken);
+            var totalRecords = await query.CountAsync(cancellationToken);
+
+            return new JsonResult(new Pagination<Supplier>(request.Page, result.Count, totalRecords, result));
+        }
+    }
+
+}
