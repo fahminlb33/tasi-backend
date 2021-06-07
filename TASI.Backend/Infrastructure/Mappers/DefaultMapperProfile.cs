40f96e8234972fecdbb14ddffffbf290e858ce1f
fahminlb33 <fahminlb33@gmail.com>
2021-06-07 08:05:12 +0700

    Add rounding digit for floating point types

---
+++ TASI.Backend/Infrastructure/Mappers/DefaultMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AutoMapper;
+
+namespace TASI.Backend.Infrastructure.Mappers
+{
+    public class DefaultMapperProfile : Profile
+    {
+        public DefaultMapperProfile()
+        {
+            CreateMap<decimal, decimal>()
+                .ConvertUsing(x => Math.Round(x, 2));
+            CreateMap<double, double>()
+                .ConvertUsing(x => Math.Round(x, 2));
+        }
+    }
+}
